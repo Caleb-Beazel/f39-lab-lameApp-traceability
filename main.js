0a273636d11dab5f268d37601a7a903a53eb0759
Caleb Beazel <caleb.beazel@gmail.com>
2023-05-18 16:13:10 -0600

    lame-app traceability lab w/ rollbar  v5

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 let button = document.querySelector('button')
 let div = document.querySelector('div')
 
+
 const buttonMessage = () => {
     
     let newMessage = document.createElement('p')
     newMessage.innerHTML = "Well, that's not very nice."
     div.appendChild(newMessage)
 
+    rollbar.info('Someone has dissed your web page.')
     
 }
 
 button.addEventListener('click', buttonMessage)
+
