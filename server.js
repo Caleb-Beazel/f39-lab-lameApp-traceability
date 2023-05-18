a35e114352569fe5782a31e863e2976ff644052b
Caleb Beazel <caleb.beazel@gmail.com>
2023-05-18 15:59:19 -0600

    lame-app traceability lab w/ rollbar  v4

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ console.log(__dirname)
 
 app.use(express.static(`${__dirname}`))
 
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '1b94a65f4756404db0747457d9e72432',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.listen(6556, () => console.log('Server up and running govenor. On port 6556.'))
