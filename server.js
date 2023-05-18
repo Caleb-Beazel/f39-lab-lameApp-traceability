6d9f587f71d1600b2c5fbb05dfb0895b58b391bb
Caleb Beazel <caleb.beazel@gmail.com>
2023-05-18 15:28:29 -0600

    lame-app traceability lab v3

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app = express()
 
 app.use(express.json())
 
+console.log(__dirname)
+
+app.use(express.static(`${__dirname}`))
+
 app.listen(6556, () => console.log('Server up and running govenor. On port 6556.'))
